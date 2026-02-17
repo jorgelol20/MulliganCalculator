5eec8c3a78ee326a0159da0878db21c08fc6cf2d
Jorge Colomer Albertos <94186347+jorgelol20@users.noreply.github.com>
2026-02-17 20:26:13 +0100

    Remove Sitemap plugin from Vite configuration
    Removed Sitemap plugin configuration from Vite config.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react(),
-  Sitemap({
-    hostnane: 'https://mulligan-calculator.vercel.app/',
-    dynamicRoutes: ['/','/results']
-  })]
+  plugins: [react()]
 })
